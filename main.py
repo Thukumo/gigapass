914599c9626e18330e918cfd1ef7e7eb6f7bb94a
つくも <92912896+Thukumo@users.noreply.github.com>
2024-08-28 16:04:36 +0900

    あ

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ async def check_dlkey(url, dlkey, is_zip, pbar):
                 if response.status == 200:
                     json = await response.json()
                     if json["status"] == 0:
-                        pbar.write(f"key = {dlkey}")
+                        pbar.write(f"Found: key(?) = {dlkey}")
                         return [0, dlkey]
                     elif json["status"] == 1:
                         return [1, dlkey]
@@ -28,6 +28,7 @@ async def check_dlkey(url, dlkey, is_zip, pbar):
                     if response.status == 503:
                         #tqdm.write(f"503エラーが発生しました。同時実行数を下げることを推奨します。試行していたキー: {dlkey}")
                         return [3, dlkey]
+                    #raise Exception(f"HTTPリクエストに失敗 status: {response.status}\n試行していたキー: {dlkey}")
                     raise Exception(f"HTTPリクエストに失敗 status: {response.status}\n試行していたキー: {dlkey}")
     except OSError:
         #raise Exception(f"HTTPリクエストに失敗 status: {response.status}\n試行していたキー: {dlkey}")
@@ -46,41 +47,46 @@ async def main():
                     tasks = [check_dlkey(url, s, is_zip, pbar) for s in tmpl]
                     res = await asyncio.gather(*tasks)
                     for r in res:
-                        pbar.update(1)
                         if r[0] == 0:
                             return True
                         elif r[0] in [2 , 3]:
                             if r[0] == 3: sleep(0.05)
-                            if (res := await check_dlkey(url, r[1], is_zip, pbar))[0] in [2, 3]: raise RuntimeError(f"2回連続で失敗しまし{"た(503)" if res[0] == 3 else "た"} 試行していたキー: {r[1]}")
+                            if (res := await check_dlkey(url, r[1], is_zip, pbar))[0] in [2, 3]: raise RuntimeError("2回連続で失敗しました"+("(503)" if res[0] == 3 else "")+f" 試行していたキー: {r[1]}")
                             elif res[0] == 0: return True
+                        pbar.update(1)
                 except StopIteration:
                     if len(tmpl) != 0:
                         tasks = [check_dlkey(url, s, is_zip, pbar) for s in tmpl]
                         res = await asyncio.gather(*tasks)
                         for r in res:
-                            pbar.update(1)
                             if r[0] == 0:
                                 return True
                             elif r[0] in [2 , 3]:
                                 if r[0] == 3: sleep(0.05)
-                                if (res := await check_dlkey(url, r[1], is_zip, pbar))[0] in [2, 3]: raise RuntimeError(f"2回連続で失敗しまし{"た(503)" if res[0] == 3 else "た"} 試行していたキー: {r[1]}")
+                                if (res := await check_dlkey(url, r[1], is_zip, pbar))[0] in [2, 3]: raise RuntimeError("2回連続で失敗しました"+("(503)" if res[0] == 3 else "")+f" 試行していたキー: {r[1]}")
                                 elif res[0] == 0: return True
+                            pbar.update(1)
                     break
     return False
 
 url = input("URLを入力してください(短縮URLはエラー吐きます): ")
-is_zip = input("zip(まとめてダウンロード)?(y/n): ").lower() == "y"
-nump = 6 #最大同時実行数
+print(f"URL: {url}\n")
+is_zip = input("zip(まとめてダウンロード)?初期値n (y/n): ").lower() == "y"
+print("")
+nump = 3
 try:
-    nump = int(input("同時実行数を入力してください。8以下をおすすめします。"))
+    nump = int(input("同時実行数を入力してください。\n503が頻発する場合この値を下げることをおすすめします。初期値3: "))
 except ValueError:
-    print("無効な値が入力されました。初期値として6を使用します。")
+    pass
+print("")
 base_str = "abcdefghijklmnopqrstuvwxyz"
 base_str += base_str.upper() + "0123456789"
-if input("数字のみ探索しますか?\n英字を探索しない代わりに、探索にかかる時間が約1/1478になります。(y/n)").lower() == "y": base_str = "0123456789"
-begin_at = input("前回エラー終了した場合は、\nここで前回最後に試行していたキーを入力してください。")
+if input("数字のみ探索しますか?\n英字を探索しない代わりに、探索にかかる時間が約1/1478になります。\n初期値y (y/n): ").lower() != "n": base_str = "0123456789"
+print("")
+begin_at = input("前回エラー終了した場合は、\nここで前回最後に試行していたキーを入力してください。: ")
+print("")
 if len(begin_at)==0: begin_at=None
-#for n in range(10):
+
 roop = True
 while roop:
     roop = False
@@ -89,12 +95,10 @@ while roop:
     except RuntimeError as e:
         roop = True
         print(e)
-        sleep(randint(1, 5))
+        if "503" in str(e):
+            print("503エラーが発生しました。30秒+α待機します。")
+            sleep(30+randint(1, 4)) #多分30秒くらい待たないとまた503吐かれる
         print(f'{ str(e).split(" ")[2] }から再試行を開始します...')
         begin_at = str(e).split(" ")[2]
-        """
-        if n == 9:
-            print("10回連続で失敗しました。終了します。\nサーバに過負荷がかかっている可能性があります。\n同時実行数を下げることを推奨します。")
-            break
-        """
-if not result: print("全てのキーを試行しましたが、正しいキーが見つかりませんでした。")
+if result: print("探索に成功しました。")
+else: print("全てのキーを試行しましたが、正しいキーが見つかりませんでした。")
